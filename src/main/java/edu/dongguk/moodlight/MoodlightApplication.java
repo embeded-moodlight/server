196a04681f88db5034be77af4345f7bc4a83aa33
윤주형 <juhyung0818@naver.com>
2017-11-27 00:44:53 +0900

    birth

---
+++ src/main/java/edu/dongguk/moodlight/MoodlightApplication.java
@@ -0,0 +1,12 @@
+package edu.dongguk.moodlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodlightApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/dongguk/moodlight/MoodlightApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.dongguk.moodlight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoodlightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
